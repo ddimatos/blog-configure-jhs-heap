b5d69d1ba0bd006a4d26fa4135439ae7b1a24523
ddimatos <dimatos@gmail.com>
2017-06-08 15:58:05 -0700

    Create jhs-cache-size.sh

--- scripts/jhs-cache-size.sh
+++ scripts/jhs-cache-size.sh
@@ -36,29 +36,29 @@ echo "Based on the job size summary $JOB_SIZE_SUM bytes and the compenstaion $CO
 ################################################################################
 # STDOUT for the above commands:
 ################################################################################
-# root@iop42-oom1 ~]# DATE="2017/02/10";
-# root@iop42-oom1 ~]# PERCENT=".20";
-# root@iop42-oom1 ~]# DIVISOR=24;
-# root@iop42-oom1 ~]# JOB_COUNT=`sudo -u hdfs hdfs dfs -ls -R /mr-history/done/$DATE |grep jhist | gawk '{n++;} END {print n;}'`;
-# root@iop42-oom1 ~]# echo "$JOB_COUNT Jobs were run on date $DATE";
+# root@localhost ~]# DATE="2017/02/10";
+# root@localhost ~]# PERCENT=".20";
+# root@localhost ~]# DIVISOR=24;
+# root@localhost ~]# JOB_COUNT=`sudo -u hdfs hdfs dfs -ls -R /mr-history/done/$DATE |grep jhist | gawk '{n++;} END {print n;}'`;
+# root@localhost ~]# echo "$JOB_COUNT Jobs were run on date $DATE";
 #                     77 Jobs were run on date 2017/02/10
-# root@iop42-oom1 ~]# JOB_CACHE_SIZE=`echo $JOB_COUNT/$DIVISOR|bc`;
-# root@iop42-oom1 ~]# echo "Based on $JOB_COUNT jobs and the divisor $DIVISOR, you should set the JHS cache size (mapreduce.jobhistory.loadedjobs.cache.size) to: $JOB_CACHE_SIZE"
+# root@localhost ~]# JOB_CACHE_SIZE=`echo $JOB_COUNT/$DIVISOR|bc`;
+# root@localhost ~]# echo "Based on $JOB_COUNT jobs and the divisor $DIVISOR, you should set the JHS cache size (mapreduce.jobhistory.loadedjobs.cache.size) to: $JOB_CACHE_SIZE"
 #                     Based on 77 jobs and the divisor 24, you should set the JHS cache size (mapreduce.jobhistory.loadedjobs.cache.size) to: 3
-# root@iop42-oom1 ~]# XML_FILE_SUM=`sudo -u hdfs hdfs dfs -ls -R /mr-history/done/$DATE |grep xml |sort -r -k 5,5|head -$JOB_CACHE_SIZE | gawk '{sum += $5;} END {print sum;}'`;
-# root@iop42-oom1 ~]# echo "The sum for the largest $JOB_CACHE_SIZE XML files for date $DATE is: $XML_FILE_SUM bytes";
+# root@localhost ~]# XML_FILE_SUM=`sudo -u hdfs hdfs dfs -ls -R /mr-history/done/$DATE |grep xml |sort -r -k 5,5|head -$JOB_CACHE_SIZE | gawk '{sum += $5;} END {print sum;}'`;
+# root@localhost ~]# echo "The sum for the largest $JOB_CACHE_SIZE XML files for date $DATE is: $XML_FILE_SUM bytes";
 #                     The sum for the largest 3 XML files for date 2017/02/10 is: 387080 bytes
-# root@iop42-oom1 ~]# JHIST_FILE_SUM=`sudo -u hdfs hdfs dfs -ls -R /mr-history/done/$DATE |grep jhist |sort -r -k 5,5|head -$JOB_CACHE_SIZE | gawk '{sum += $5;} END {print sum;}'`
-# root@iop42-oom1 ~]# echo "The sum for the largest $JOB_CACHE_SIZE JHIST files for date $DATE is: $JHIST_FILE_SUM bytes";
+# root@localhost ~]# JHIST_FILE_SUM=`sudo -u hdfs hdfs dfs -ls -R /mr-history/done/$DATE |grep jhist |sort -r -k 5,5|head -$JOB_CACHE_SIZE | gawk '{sum += $5;} END {print sum;}'`
+# root@localhost ~]# echo "The sum for the largest $JOB_CACHE_SIZE JHIST files for date $DATE is: $JHIST_FILE_SUM bytes";
 #                     The sum for the largest 3 JHIST files for date 2017/02/10 is: 100585 bytes
-# root@iop42-oom1 ~]# JOB_SIZE_SUM=`echo $XML_FILE_SUM + $JHIST_FILE_SUM|bc`;
-# root@iop42-oom1 ~]# echo "The sum for the largest $JOB_CACHE_SIZE job logs is: $JOB_SIZE_SUM bytes"
+# root@localhost ~]# JOB_SIZE_SUM=`echo $XML_FILE_SUM + $JHIST_FILE_SUM|bc`;
+# root@localhost ~]# echo "The sum for the largest $JOB_CACHE_SIZE job logs is: $JOB_SIZE_SUM bytes"
 #                     The sum for the largest 3 job logs is: 487665 bytes
-# root@iop42-oom1 ~]# COMPENSATION_FACTOR=`echo $JOB_SIZE_SUM*$PERCENT|bc| awk '{print int($1+0.9)}'`;
-# root@iop42-oom1 ~]# echo "Based on the job summary $JOB_SIZE_SUM bytes * $PERCENT varience, the compensation factor for large jobs is: $COMPENSATION_FACTOR bytes"
+# root@localhost ~]# COMPENSATION_FACTOR=`echo $JOB_SIZE_SUM*$PERCENT|bc| awk '{print int($1+0.9)}'`;
+# root@localhost ~]# echo "Based on the job summary $JOB_SIZE_SUM bytes * $PERCENT varience, the compensation factor for large jobs is: $COMPENSATION_FACTOR bytes"
 #                     Based on the job summary 487665 bytes * .20 varience, the compensation factor for large jobs is: 97533 bytes
-# root@iop42-oom1 ~]# HEAP=`echo $JOB_SIZE_SUM+$COMPENSATION_FACTOR|bc`;
-# root@iop42-oom1 ~]# echo "Based on the job size summary $JOB_SIZE_SUM bytes and the compenstaion $COMPENSATION_FACTOR bytes, the JHS HEAP should be set to: $HEAP bytes"
+# root@localhost ~]# HEAP=`echo $JOB_SIZE_SUM+$COMPENSATION_FACTOR|bc`;
+# root@localhost ~]# echo "Based on the job size summary $JOB_SIZE_SUM bytes and the compenstaion $COMPENSATION_FACTOR bytes, the JHS HEAP should be set to: $HEAP bytes"
 #                         Based on the job size summary 487665 bytes and the compenstaion 97533 bytes, the JHS HEAP should be set to: 585198 bytes
                                   
 #########################################################
