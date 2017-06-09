9647093a8e12bc560a86f9f95bf73107473f6a8a
ddimatos <dimatos@gmail.com>
2017-06-08 22:18:01 -0700

    Create jhs-cache-size.sh
    Added echo stmt to help with setting heap with Ambari

--- scripts/jhs-cache-size.sh
+++ scripts/jhs-cache-size.sh
@@ -42,35 +42,18 @@ echo "Based on the job summary $JOB_SIZE_SUM bytes * $PERCENT varience, the comp
 #Add SUM and FACTOR together, this is your new heap size
 HEAP=`echo $JOB_SIZE_SUM+$COMPENSATION_FACTOR|bc`; 
 echo "Based on the job size summary $JOB_SIZE_SUM bytes and the compenstaion $COMPENSATION_FACTOR bytes, the JHS HEAP should be set to: $HEAP bytes"
-
+echo "Set variable HADOOP_JOB_HISTORYSERVER_HEAPSIZE in mapred-site.xml to $HEAP bytes navigating Ambari > MapReduce2 > Configs > Advanced > History Server > History Server heap size" 
 ################################################################################
 # STDOUT for the above commands:
 ################################################################################
-# root@localhost ~]# DATE="2017/02/10";
-# root@localhost ~]# PERCENT=".20";
-# root@localhost ~]# DIVISOR=24;
-# root@localhost ~]# JOB_COUNT=`sudo -u hdfs hdfs dfs -ls -R /mr-history/done/$DATE |grep jhist | gawk '{n++;} END {print n;}'`;
-# root@localhost ~]# echo "$JOB_COUNT Jobs were run on date $DATE";
-#                     77 Jobs were run on date 2017/02/10
-# root@localhost ~]# JOB_CACHE_SIZE=`echo $JOB_COUNT/$DIVISOR|bc`;
-# root@localhost ~]# echo "Based on $JOB_COUNT jobs and the divisor $DIVISOR, you should set the JHS cache size (mapreduce.jobhistory.loadedjobs.cache.size) to: $JOB_CACHE_SIZE"
-#                     Based on 77 jobs and the divisor 24, you should set the JHS cache size (mapreduce.jobhistory.loadedjobs.cache.size) to: 3
-# root@localhost ~]# XML_FILE_SUM=`sudo -u hdfs hdfs dfs -ls -R /mr-history/done/$DATE |grep xml |sort -r -k 5,5|head -$JOB_CACHE_SIZE | gawk '{sum += $5;} END {print sum;}'`;
-# root@localhost ~]# echo "The sum for the largest $JOB_CACHE_SIZE XML files for date $DATE is: $XML_FILE_SUM bytes";
-#                     The sum for the largest 3 XML files for date 2017/02/10 is: 387080 bytes
-# root@localhost ~]# JHIST_FILE_SUM=`sudo -u hdfs hdfs dfs -ls -R /mr-history/done/$DATE |grep jhist |sort -r -k 5,5|head -$JOB_CACHE_SIZE | gawk '{sum += $5;} END {print sum;}'`
-# root@localhost ~]# echo "The sum for the largest $JOB_CACHE_SIZE JHIST files for date $DATE is: $JHIST_FILE_SUM bytes";
-#                     The sum for the largest 3 JHIST files for date 2017/02/10 is: 100585 bytes
-# root@localhost ~]# JOB_SIZE_SUM=`echo $XML_FILE_SUM + $JHIST_FILE_SUM|bc`;
-# root@localhost ~]# echo "The sum for the largest $JOB_CACHE_SIZE job logs is: $JOB_SIZE_SUM bytes"
-#                     The sum for the largest 3 job logs is: 487665 bytes
-# root@localhost ~]# COMPENSATION_FACTOR=`echo $JOB_SIZE_SUM*$PERCENT|bc| awk '{print int($1+0.9)}'`;
-# root@localhost ~]# echo "Based on the job summary $JOB_SIZE_SUM bytes * $PERCENT varience, the compensation factor for large jobs is: $COMPENSATION_FACTOR bytes"
-#                     Based on the job summary 487665 bytes * .20 varience, the compensation factor for large jobs is: 97533 bytes
-# root@localhost ~]# HEAP=`echo $JOB_SIZE_SUM+$COMPENSATION_FACTOR|bc`;
-# root@localhost ~]# echo "Based on the job size summary $JOB_SIZE_SUM bytes and the compenstaion $COMPENSATION_FACTOR bytes, the JHS HEAP should be set to: $HEAP bytes"
-#                         Based on the job size summary 487665 bytes and the compenstaion 97533 bytes, the JHS HEAP should be set to: 585198 bytes
-                                  
+# 29 Jobs were run on date 2017/06/08
+# Based on 29 jobs and the divisor 3, you should set the JHS cache size (mapreduce.jobhistory.loadedjobs.cache.size) to: 9
+# The sum for the largest 9 XML files for date 2017/06/08 is: 1148175 bytes
+# The sum for the largest 9 JHIST files for date 2017/06/08 is: 300574 bytes
+# The sum for the largest 9 job logs is: 1448749 bytes
+# Based on the job summary 1448749 bytes * .20 varience, the compensation factor for large jobs is: 289750 bytes
+# Based on the job size summary 1448749 bytes and the compenstaion 289750 bytes, the JHS HEAP should be set to: 1738499 bytes
+    
 #########################################################
 # IGNORE STEP 3, this is a work in progress for IOP 4.3 - Hadoop 2.7.3
 #########################################################
