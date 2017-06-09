5248ca981e1b8fd1d0f027116268e8798da64873
ddimatos <dimatos@gmail.com>
2017-06-09 00:05:25 -0700

    Create jhs-cache-size.sh

--- scripts/jhs-cache-size.sh
+++ scripts/jhs-cache-size.sh
@@ -64,19 +64,19 @@ echo "Set variable HADOOP_JOB_HISTORYSERVER_HEAPSIZE in mapred-site.xml to $HEAP
 #Count all the tasks used on $DATE
 TASK_COUNT=`for F in \`sudo -u hdfs hdfs dfs  -ls -R /mr-history/done/$DATE |grep jhist |awk '{print $8}'\`;do sudo -u hdfs hdfs dfs -cat $F |grep -i \"totalMaps\": | python -c 'import sys, json; print json.load(sys.stdin)["event"]["org.apache.hadoop.mapreduce.jobhistory.JobInited"]["totalMaps"]';done|{ tr '\n' +; echo 0;}|bc`;
 
-#Divide the number of jobs you run by a divisor (a number that equates to something meaningful, ie , 24 is mine for 24 hours, so i am going to cache only the last hour of jobs) 
+#Divide the number of task you run by a divisor (a number that equates to something meaningful, ie , 24 is mine for 24 hours, so i am going to cache only the last hour of jobs) 
 TASK_CACHE_SIZE=`echo $TASK_COUNT/$DIVISOR|bc`;
 echo "Based on $TASK_COUNT tasks and the divisor $DIVISOR, you should set the JHS task cache size (mapreduce.jobhistory.loadedtasks.cache.size) to: $TASK_CACHE_SIZE"
       
-#Sum the largest XML and JHST files 
+#Multiply the task cache size by a historical task size (15 kb) to get a rough heap estimate before the compensation
 TASK_SIZE_KB=15
 TASK_SIZE_SUM=`echo $TASK_CACHE_SIZE*$TASK_SIZE_KB|bc`;
 
-#Multiply file size sum by 20% to compensate for unplanned large jobs
+#Multiply task cache  size sum by 20% to compensate for unplanned large jobs
 COMPENSATION_FACTOR=`echo $TASK_SIZE_SUM*$PERCENT|bc| awk '{print int($1+0.9)}'`; 
 echo "Based on the task count $TASK_COUNT * $TASK_SIZE_KB task size * $PERCENT varience, the compensation factor is: $COMPENSATION_FACTOR bytes"
             
-#Add SUM and FACTOR together, this is your new heap size
+# Sum the task size and compensation factor together, this is your new heap size
 HEAP=`echo $TASK_SIZE_SUM+$COMPENSATION_FACTOR|bc`; 
 echo "Based on the task size summary $TASK_SIZE_SUM bytes and the compenstaion $COMPENSATION_FACTOR bytes, the JHS HEAP should be set to: $HEAP bytes"
 echo "Set variable HADOOP_JOB_HISTORYSERVER_HEAPSIZE in mapred-site.xml to $HEAP bytes navigating Ambari > MapReduce2 > Configs > Advanced > History Server > History Server heap size" 
