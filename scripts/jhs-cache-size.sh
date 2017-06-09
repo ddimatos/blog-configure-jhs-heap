4d92a73cbd1f4724b5bbb37ced0de24e75122798
ddimatos <dimatos@gmail.com>
2017-06-08 21:39:30 -0700

    Create jhs-cache-size.sh

--- scripts/jhs-cache-size.sh
+++ scripts/jhs-cache-size.sh
@@ -1,5 +1,15 @@
-
-#NOTE: This script is a work in progress, it will be some more time before its ready for use.
+#!/bin/sh
+##########################################################################################
+# Script will review the log files (.jhst & .xml) to suggest a heap
+# size for the JHS. 
+#
+# Variables require to be set:
+# DATE is the day you wish to capture the files for, jobs are usually located 
+#      on HDFS with a date directory structure where files are located in HDFS
+#      at /mr-history/done/$DATE 
+# PERCENT is the over compensating factor needed for large unanticipated jobs.
+# DIVISOR is the how many iterations you want to divide the number of jobs into. 
+##########################################################################################
 
 #Count all the .jhst and .xml files for a day to get an idea of the number of jobs you run
 DATE="2017/02/10";
@@ -64,35 +74,4 @@ echo "Based on the job size summary $JOB_SIZE_SUM bytes and the compenstaion $CO
 #########################################################
 # IGNORE STEP 3, this is a work in progress for IOP 4.3 - Hadoop 2.7.3
 #########################################################
-3) Well there is another property that by default is NOT enabled until it is provided a positive number, 
-   that property mapreduce.jobhistory.loadedtasks.cache.size and controls the JHS server by a TASK count. 
-   So now you don't control your JHS by jobs, you configure it by deciding how many tasks should be retained
-   for displaying in the JHS UI.
 
-   If the total number of tasks loaded exceeds this value, then the job cache will be shrunk down until 
-   it is under this limit (minimum 1 job in cache). If this value is empty or nonpositive then the cache 
-   reverts to using the property mapreduce.jobhistory.loadedjobs.cache.size as a job cache size. 
-
-   (from mapred-default.xml)
-   The 2 recommendations for the mapreduce.jobhistory.loadedtasks.cache.size property are:
-   a) For every 100k of cache size, set the heap size of the Job History Server to 1.2GB. 
-      For example, mapreduce.jobhistory.loadedtasks.cache.size=500000, heap size=6GB.
-   b) Make sure that the cache size is larger than the number of tasks required for the largest job run on the cluster. 
-      It might be a good idea to set the value slightly higher (say, 20%) in order to allow for job size growth. 
-
-   Now you are probably wondering how to calculate the number of tasks for the past day such that you can come up
-   with a reasonable task cache size. If you review review your jobs for the day in HDFS, you can calculate the 
-   number of tasks. Each job has JSON used by JHS and there is a key and value pair indicating the number
-   of tasks, this is totalMaps:16, so here the key is totalMaps  and the value 16 (16 tasks)
-
-   Running this command find parse all job log files for a given day, extract the totalMaps JSON , parse 
-   it for the value of tasks and then sum the values. These values are the total maps tasks , when summed, 
-   they account for all tasks for a given day which you can then use to set mapreduce.jobhistory.loadedtasks.cache.size.
-
-   for F in `sudo -u hdfs hdfs dfs  -ls -R /mr-history/done/2017/02/10 |grep jhist |awk '{print $8}'`;do sudo -u hdfs hdfs dfs -cat $F |grep -i \"totalMaps\": | python -c 'import sys, json; print json.load(sys.stdin)["event"]["org.apache.hadoop.mapreduce.jobhistory.JobInited"]["totalMaps"]';done|{ tr '\n' +; echo 0; }|bc
-
-   After you have the total number of map tasks for a day, you can then decide how many you want to cache, 
-   if you choose  you want 500000 tasks, mapreduce.jobhistory.loadedtasks.cache.size=500000, then you need to 
-   adjust your heap to accommodate each task size. JIRA MAPREDUCE-6622 recommends setting HEAP to  1.2GB + 20% which 
-   averages each task about 16k, I have found through testing that most tasks are closer to 30k. I covered 
-   how to measure this in the first step. 
